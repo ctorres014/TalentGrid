2affc7345d64d2a248ab6da003b0084c8be0c632
claudio <ctorres014@gmail.com>
2026-03-02 06:57:17 -0300

    add new talent controller with action search

---
+++ TalentGrid.Api/Controllers/TalentController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TalentGrid.Application.Contracts.Dto;
+using TalentGrid.Application.Contracts.UseCase;
+
+namespace TalentGrid.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TalentController: ControllerBase
+    {
+        private readonly ISearchTalent _searchTalen;
+
+        public TalentController(ISearchTalent searchTalent)
+        {
+            _searchTalen = searchTalent;
+        }
+
+        [HttpGet("search")]
+        public async Task<IActionResult> Search([FromQuery] string skillName, [FromQuery] int minLevel= 1) 
+        {
+            var result = await _searchTalen.SearchTalents(skillName, minLevel);
+            if (result == null || !result.Any())
+                return NotFound("No talents found matching the criteria.");
+            return Ok(result);
+        }
+    }
+}
